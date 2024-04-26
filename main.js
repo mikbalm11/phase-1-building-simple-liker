04458b0397012b9add3dfc061d9f17a49ccf3e8a
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-04-25 22:02:28 -0400

    lab complete

--- main.js
+++ main.js
@@ -2,10 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.getElementById('modal');
 
+  const handleLike = async (event) => {
+    const heartGlyph = event.target;
+    if (heartGlyph.classList.contains('like-glyph')) {
+      if (heartGlyph.classList.contains('activated-heart')) {
+          try {
+              await mimicServerCall();
+              heartGlyph.classList.remove('activated-heart');
+              heartGlyph.textContent = EMPTY_HEART;
+              errorModal.classList.add('hidden');
+          } catch (error) {
+              const modalMessage = document.getElementById('modal-message');
+              modalMessage.textContent = error;
+              errorModal.classList.remove('hidden');
+              setTimeout(() => errorModal.classList.add('hidden'), 3000);
+          }
+      } else {
+          try {
+              await mimicServerCall();
+              heartGlyph.classList.add('activated-heart');
+              heartGlyph.textContent = FULL_HEART;
+              errorModal.classList.add('hidden');
+          } catch (error) {
+              const modalMessage = document.getElementById('modal-message');
+              modalMessage.textContent = error;
+              errorModal.classList.remove('hidden');
+              setTimeout(() => errorModal.classList.add('hidden'), 3000);
+          }
+      }
+    }
+  };
 
-
+  document.body.addEventListener('click', handleLike);
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
